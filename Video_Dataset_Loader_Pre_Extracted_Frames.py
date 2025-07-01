b6981928d97624116371f378097ce1c7b443cffc
Naneet <157294737+Naneet@users.noreply.github.com>
2025-07-02 00:27:54 +0530

    Create Video_Dataset_Loader_Pre_Extracted_Frames.py

---
+++ Video_Dataset_Loader_Pre_Extracted_Frames.py
@@ -0,0 +1,86 @@
+import torch
+from torchvision.transforms.functional import to_tensor
+from torch.utils.data import Dataset
+
+import os
+import subprocess
+from PIL import Image
+import shutil
+import sys
+import numpy as np
+
+# Use this function to load the data path and label for the dataset
+def data_and_words(path):
+    words = os.listdir(path)
+    words_list = []
+    data = []
+    for word in words:
+        words_list.append(word)
+        word_videos_path = os.path.join(path, word)
+        videos = os.listdir(word_videos_path)
+        for video in videos:
+            data.append((os.path.join(word_videos_path, video), len(words_list)-1))
+
+    words_dict = dict({})
+    for num, word in enumerate(words_list):
+        words_dict[word] = num
+
+    return data, words_list, words_dict  # data will return a list of tuples where data[0] is the path to the video and data[1] is the label
+                                         # words_list can be used to convert index to word
+                                         # words_dict can be used to convert word to index
+
+
+
+
+class VideoDataset(Dataset):
+    def __init__(self, data, temp_data_folder, NUM_FRAMES=10, transform_frame=None,transform_video=None):
+        self.data = data  # It should be a list of tuples where data[0] is the path to the video and data[1] is the label
+
+        self.transform_frame = transform_frame  # Transformations to be done on the individual frames.
+                                                # Recommended to use when transforms is required at frames level with some randomness, eg: Random Crop
+                                                # Note: If the Dataset is showing tensor issue, try adding `ToTensor()` in transform.
+
+        self.transform_video = transform_video  # Transformations to be done on the whole video.
+                                                # Recommended to use when transforms is required at video level with some randomness, eg: Random Horizontal Flip
+
+        self.NUM_FRAMES = NUM_FRAMES  # Number of frames to be extracted from the video
+
+
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+
+        video_path = self.data[idx][0]
+
+        video_frames = []
+
+        video_images = sorted(os.listdir(video_path))
+
+        frame_positions = np.linspace(0, end, self.max_frames, endpoint=False)
+
+        # Selecting the NUM_FRAMES from the video
+        for n in frame_positions:
+            img = video_images[int(n)]
+            img_path = os.path.join(output_video_path, img)
+            with Image.open(img_path) as pil_img:
+                if self.transform_frame:
+                    video_frames.append(self.transform_frame(pil_img))  # Note: Try adding ToTensor() in transform_frame, if any tensor related error arrises.
+                    
+                else:
+                    video_frames.append(to_tensor(pil_img))
+
+        try:
+            video_frames = torch.stack(video_frames)  # Note: Try adding ToTensor() in transform_frame, if any tensor related error arrises.
+        except TypeError:
+            print(f"TypeError: Tried to stack {type(video_frames[0])}. Add ToTensor() in transform_frame!")
+            sys.exit(1)
+            return None, None
+            
+
+        if self.transform_video:
+            video_frames = self.transform_video(video_frames)
+
+        label = self.data[idx][1]
+        return video_frames, label
